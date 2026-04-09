623cd44141e2a4110af3c78dbf1d15f9ace82246
Karol Arvizu <lorakarvizu@gmail.com>
2026-04-08 21:33:07 -0600

    Primer commit

---
+++ src/main/java/com/example/ticketinios/ms_groups/MsGroupsApplication.java
@@ -0,0 +1,13 @@
+package com.example.ticketinios.ms_groups;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MsGroupsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MsGroupsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/ticketinios/ms_groups/MsGroupsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.ticketinios.ms_groups;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MsGroupsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
